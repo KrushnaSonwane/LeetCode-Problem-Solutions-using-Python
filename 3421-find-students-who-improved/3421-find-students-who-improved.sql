0482cc66d1ac939acfa549f2170e313c69821896
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2026-02-01 19:27:30 +0530

    Time: 296 ms (41.36%), Space: 0B (100%) - LeetHub

--- 3421-find-students-who-improved/3421-find-students-who-improved.sql
+++ 3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -1,17 +1,11 @@
 # Write your MySQL query statement below
-with a as (select ss.* from(
-select s.*,
-    row_number() over(partition by s.student_id, s.subject order by s.exam_date) as rn
-from Scores s) ss 
-where ss.rn = 1),
-
-b as (select ss.* from(
-select s.*,
-    row_number() over(partition by s.student_id, s.subject order by s.exam_date desc) as rn
-from Scores s) ss 
-where ss.rn = 1)
-
-select a.student_id, a.subject, a.score as first_score, b.score as latest_score from a
-inner join b
-on a.student_id = b.student_id and b.subject = a.subject
-where a.score < b.score
+with tmp as(
+    select
+        student_id,
+        subject,
+        first_value(score) over(partition by student_id, subject order by exam_date) as first_score,
+        first_value(score) over(partition by student_id, subject order by exam_date desc) as latest_score
+    from Scores
+)
+select distinct * from tmp
+where first_score < latest_score
