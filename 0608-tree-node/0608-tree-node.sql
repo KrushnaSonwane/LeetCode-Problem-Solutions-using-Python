dc44784dc9ecc885eb4d7212bd0ac204d744d228
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2026-02-02 17:27:52 +0530

    Time: 487 ms (31.09%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -3,7 +3,12 @@ select
     t.id,
     case
         when t.p_id is null then "Root"
-        when (select count(*) from Tree where p_id = t.id) = 0 then "Leaf"
+        when (
+            select 
+                count(*) 
+            from Tree 
+            where p_id = t.id
+        ) = 0 then "Leaf"
         else "Inner"
     end as type
 from Tree t
