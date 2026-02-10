af31f1ad92d2bd335c9d7fb61ba1966b13d1b385
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2026-02-10 19:01:32 +0530

    Time: 277 ms (91.98%), Space: 0B (100%) - LeetHub

--- 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -1,19 +1,21 @@
 # Write your MySQL query statement below
-select * from(
-select
-    u.user_id,
-    round(avg(
-        case
-            when u.activity_type = 'free_trial' then u.activity_duration
-        end
-    ), 2) as trial_avg_duration,
-    round(avg(
-        case
-            when u.activity_type = 'paid' then u.activity_duration
-        end
-    ), 2) as paid_avg_duration 
-from UserActivity u
-group by u.user_id
-) a
-where paid_avg_duration is not null and trial_avg_duration is not null
-order by user_id
+SELECT 
+    * 
+FROM(
+    SELECT
+        user_id,
+        ROUND(AVG(
+            CASE
+                WHEN activity_type = 'free_trial' THEN activity_duration
+            END
+        ), 2) AS trial_avg_duration,
+        ROUND(AVG(
+            CASE
+                WHEN activity_type = 'paid' THEN activity_duration
+            end
+        ), 2) AS paid_avg_duration 
+    FROM UserActivitY
+    GROUP BY user_id
+) AS a
+WHERE paid_avg_duration IS NOT NULL
+ORDER BY user_id
