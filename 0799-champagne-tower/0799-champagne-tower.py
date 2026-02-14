13ce286431ee1b8354f59a6509514a90ff155eb4
Krushna Sonwane <99010640+KrushnaSonwane@users.noreply.github.com>
2026-02-14 10:56:56 +0530

    Time: 146 ms (9.55%), Space: 19.5 MB (50.36%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.py
@@ -0,0 +1,14 @@
+class Solution:
+    def champagneTower(self, poured: int, row: int, num: int) -> float:
+        if row == 0: return min(poured, 1)
+        res = [float(poured)]
+        for i in range(row):
+            new_row = [0 for _ in range(len(res) + 1)]
+            for j in range(0, len(new_row)-1):
+                curr = res[j] - 1
+                if curr > 0:
+                    new_row[j] += curr / 2
+                    new_row[j+1] += curr / 2
+            print(new_row)
+            res = new_row
+        return min(res[num], 1)
